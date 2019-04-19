58036838a53684a4831b6a34c74b9b2aa87cf635
King <47883824+KingKingGreat@users.noreply.github.com>
2019-04-19 10:09:07 +0700

    example.java deleted
    Moved to JavaCodes folder

--- example.java
+++
@@ -1,8 +0,0 @@
-// Basic Java Code By KingLordXD
-
-// Prints "Hello World !" to the console
-public class MyClass {
-  public static void main(String[] args) {
-    System.out.println("Hello World !");
-  }
-}
