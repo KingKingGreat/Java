c5cff60abf3f63c339c615594db4be8efcc1a5c2
King <47883824+KingKingGreat@users.noreply.github.com>
2019-04-19 10:08:24 +0700

    JavaCodes Folder & example.java Created
    JavaFolder & example.java created

---
+++ JavaCodes/example.java
@@ -0,0 +1,8 @@
+// Example Java Code By KingLordXD
+
+// Prints "Hello World" to the console
+public class MyClass {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
